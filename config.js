08c501e85aa7981d2024d4ff6ffbf49e54e881a7
Chenlan Yu <chenlan.yu@hotmail.com>
2026-04-15 01:29:52 -0400

    release adjustments

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Mapbox access token — kept out of git via .gitignore
-window.MAPBOX_TOKEN = 'pk.eyJ1IjoiZGVhbHRoeSIsImEiOiJjbWx4amZyaDUwc3NpM2Zwb2N4bTRoNG04In0.pK9O---Bq9-ZJtG4wAHhwA';
+window.MAPBOX_TOKEN = 'pk.eyJ1IjoiZ2VvZ3JhcGhpY3MiLCJhIjoiY21sZmNubGdrMDBoajNkb3JzcW0xY2N0NCJ9.c78raFMOuXb2PgaNjkW6pg';
